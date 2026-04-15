cee275dda115efea59364871bbb01218802ce5de
Sohelrana2815 <sohel152302@gmail.com>
2026-04-15 09:04:52 +0600

    feat: initial commit

---
+++ src/app/(pay-note)/layout.tsx
@@ -0,0 +1,18 @@
+import Footer from "@/components/shared/footer";
+import Navbar from "@/components/shared/navbar";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      {/* Main content area constrained to max-w-7xl */}
+      <main className="grow w-full max-w-7xl mx-auto px-6 py-8">
+        {children}
+      </main>
+      {/* Footer is full width by default */}
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/components/shared/footer.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="w-full">
+      {/* This inner div keeps the content centered and constrained  */}
+
+      <p className="block text-sm text-body sm:text-center">
+        &copy; 2026 QuickBill. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
